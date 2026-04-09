0bc4748f727fe2c1dc2d84377019a721ed92a312
Everton Fr. <everton7fr@gmail.com>
2026-04-08 23:01:55 -0300

    feat: GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 
@@ -235,6 +236,13 @@ export default function RootLayout({
         className={`${inter.variable} min-h-full flex flex-col antialiased`}
       >
         <Providers>{children}</Providers>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YNW9TN0T8H"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-YNW9TN0T8H');`}
+        </Script>
       </body>
     </html>
   );
